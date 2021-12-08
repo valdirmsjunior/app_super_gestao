b26ee568ec4df8b966c01eb431dd4b63277b3dd5
Valdir Júnior <valdirmsjunior@gmail.com>
2021-12-07 21:29:59 -0300

    testando

---
+++ resources/views/site/principal.blade.php
@@ -0,0 +1,1 @@
+<h3>Princiapal (view)</h3>
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'PrincipalController@principal');
+
+Route::get('/sobre-nos', 'SobreNosController@sobreNos');
+
+Route::get('/contato', 'ContatoController@contato');
+
+// Route::get('/sobre-nos', function () {
+//     return 'Sobre nós';
+// });
+
+// Route::get('/contato', function () {
+//     return 'Contato';
+// });
